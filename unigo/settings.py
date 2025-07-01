84a66d66f58bbc67864aaf92797d6717ac3a2ab3
lautaro nicanor araya vera <lautaro.araya69420@gmail.com>
2025-07-01 01:19:24 -0400

    email local

--- unigo/settings.py
+++ unigo/settings.py
@@ -127,7 +127,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "core.Usuario"
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'unimovuczc@gmail.com'
+EMAIL_HOST_PASSWORD = 'tpky lrjl sqod ppxq'
+DEFAULT_FROM_EMAIL = 'unimovuczc@gmail.com'
 
 
 
